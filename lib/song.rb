95ab55cfe49b78679f0d0e6bc8d3ed158348d072
Diogo Ide <diogoide@gmail.com>
2018-05-13 18:21:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,16 +4,17 @@ class Song
   attr_accessor :name
   attr_reader :artist
   extend Memorable::MethodClass
+  extend Findable
 
   @@songs = []
 
   def initialize
     @@songs << self
   end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  #
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def artist=(artist)
     @artist = artist
